038c7a723cc577cf61c2b0a0924b141b1c2f0505
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-06-19 14:16:46 -0700

    create the Express server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-Server
+#!/usr/bin/node
+
+const express = require("express");
+const router = require("./routes/index");
+
+// Create server object and set the path to specific port
+const server = express();
+const PORT = process.env.PORT ? process.env.PORT : 5000;
+
