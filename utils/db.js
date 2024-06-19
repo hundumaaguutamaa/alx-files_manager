ab3a074b1384cc30ff431ed8331901a5fa21d748
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-19 23:55:42 +0300

    Indentation  corrected

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-
 // Importing mongodb package to access all components
-
 const { MongoClient } = require('mongodb');
 const mongo = require('mongodb');
 const { pwdHashed } = require('./utils');
@@ -10,9 +8,9 @@ const { pwdHashed } = require('./utils');
 // Reads environment variables for the MongoDB host, port, and database name and set defaults
 class DBClient {
   constructor() {
-    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
-    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
-    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
     const dbUrl = `mongodb://${host}:${port}`;
     this.connected = false;
     this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
@@ -20,15 +18,13 @@ class DBClient {
       this.connected = true;
     }).catch((err) => console.log(err.message));
   }
-}
 
-
-isAlive() {
+  isAlive() {
     return this.connected;
   }
 
-// function retrieves the total number of documents (records) in the MongoDB collection named “users.”
-async nbUsers() {
+  // Function retrieves the total number of documents (records) in the MongoDB collection named “users.”
+  async nbUsers() {
     await this.client.connect();
     const users = await this.client.db(this.database).collection('users').countDocuments();
     return users;
@@ -36,6 +32,11 @@ async nbUsers() {
 
   async nbFiles() {
     await this.client.connect();
-    const users = await this.client.db(this.database).collection('files').countDocuments();
-    return users;
+    const files = await this.client.db(this.database).collection('files').countDocuments();
+    return files;
   }
+}
+
+// Export an instance of DBClient
+const dbClient = new DBClient();
+module.exports = dbClient;
