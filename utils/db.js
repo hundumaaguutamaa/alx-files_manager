082493ee75f023a2fdc064aa8569280929761696
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-19 23:32:46 +0300

    Reads environment variables for the MongoDB host, port, and database name

--- utils/db.js
+++ utils/db.js
@@ -7,11 +7,21 @@ const { MongoClient } = require('mongodb');
 const mongo = require('mongodb');
 const { pwdHashed } = require('./utils');
 
+// Reads environment variables for the MongoDB host, port, and database name and set defaults
 class DBClient {
   constructor() {
-	const host = process.env.DB_HOST || 'localhost';
-	const port = process.env.DB_PORT || 27017;
-	const database = process.env.DB_DATABASE || 'files_manager';
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
+    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
+    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    const dbUrl = `mongodb://${host}:${port}`;
+    this.connected = false;
+    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
+  }
+}
+
 
 isAlive() {
     return this.connected;
