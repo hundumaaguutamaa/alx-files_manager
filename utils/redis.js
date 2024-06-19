8362729283931a308925c46859b9e3ed135ae975
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-06-19 08:37:33 -0700

    contains the class RedisClient

---
+++ utils/redis.js
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+
+const { createClient } = require('redis');
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (err) => console.log(err));
+        this.connected = false;
+        this.client.on('connect', () => {
+            this.connected = true;
+        });
+    }
+
+    isAlive() {
+        return this.connected;
+    }
+
+    async get(key) {
+        try {
+            const val = await this.client.get(key);
+            return val;
+        } catch (err) {
+            console.error('Error getting key:', err);
+        }
+    }
+
+    async set(key, val, dur) {
+        try {
+            await this.client.setEx(key, dur, val);
+            console.log(`Key ${key} set with value ${val} for ${dur} seconds`);
+        } catch (err) {
+            console.error('Error setting key:', err);
+        }
+    }
+
+    async del(key) {
+        try {
+            await this.client.del(key);
+            console.log(`Key ${key} deleted`);
+        } catch (err) {
+            console.error('Error deleting key:', err);
+        }
+    }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
+
