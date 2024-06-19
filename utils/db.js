f03a95f9aa6f94ce93cfaf8f5a1c91c3c4621dd9
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-06-19 13:20:04 -0700

    Updating database environments

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
+
+// Importing mongodb package to access all components
+
 const { MongoClient } = require('mongodb');
 const mongo = require('mongodb');
 const { pwdHashed } = require('./utils');
 
 class DBClient {
   constructor() {
+	const host = process.env.DB_HOST || 'localhost';
+	const port = process.env.DB_PORT || 27017;
+	const database = process.env.DB_DATABASE || 'files_manager';
 
+isAlive() {
+    return this.connected;
   }
-
-
-	 isAlive() {
-    	 	return this.connected;
-
-
-const dbClient = new DBClient();
-module.exports = dbClient;
