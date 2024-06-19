20e1fd3003e78bf988cfbaf77b185745662c98f6
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-20 00:44:55 +0300

    Update Appcontrollers getstatus and getstats

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,30 @@
-App controller
+#!/usr/bin/node
+
+const express = require('express');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    } else {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+class AppController {
+  static async getStats(req, res) {
+    try {
+      const users = await dbClient.nbUsers();
+      const files = await dbClient.nbFiles();
+      res.status(200).json({ users, files });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+
+module.exports = AppController;
