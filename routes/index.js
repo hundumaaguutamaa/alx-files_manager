d80be08b44b9433926984ee071f178f22805b136
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-22 05:18:10 +0300

    Added put publish/unpublish endpoint

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,12 @@ const FilesController = require('../controllers/FilesController');
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe); 
+router.get('/users/me', UsersController.getMe); // Adjusted to use UsersController
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/files', FilesController.postUpload); 
+router.post('/files', FilesController.postUpload); // Add new endpoint
+router.put('/files/:id/publish', FilesController.putPublish); // Add new endpoint
+router.put('/files/:id/unpublish', FilesController.putUnpublish); // Add new endpoint
+router.get('/files/:id/data', FilesController.getFile); // Add new endpoint
 
 module.exports = router;
