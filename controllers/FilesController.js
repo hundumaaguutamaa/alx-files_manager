5c6f6eed7612876aaadb8658ea8981e735b46e24
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-22 05:19:11 +0300

    Updated publish unpublish implementation

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const mime = require('mime-types');
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 const mongo = require('mongodb');
@@ -62,6 +63,82 @@ class FilesController {
       const result = await dbClient.db.collection('files').insertOne(fileDocument);
       return res.status(201).json({ id: result.insertedId, ...fileDocument });
     }
+  }
+
+  static async putPublish(req, res) {
+    const token = req.headers['x-token'];
+    const key = `auth_${token}`;
+
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const fileId = req.params.id;
+    const file = await dbClient.db.collection('files').findOne({ _id: new mongo.ObjectID(fileId), userId: new mongo.ObjectID(userId) });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    await dbClient.db.collection('files').updateOne({ _id: new mongo.ObjectID(fileId) }, { $set: { isPublic: true } });
+    const updatedFile = await dbClient.db.collection('files').findOne({ _id: new mongo.ObjectID(fileId) });
+
+    return res.status(200).json(updatedFile);
+  }
+
+  static async putUnpublish(req, res) {
+    const token = req.headers['x-token'];
+    const key = `auth_${token}`;
+
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const fileId = req.params.id;
+    const file = await dbClient.db.collection('files').findOne({ _id: new mongo.ObjectID(fileId), userId: new mongo.ObjectID(userId) });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    await dbClient.db.collection('files').updateOne({ _id: new mongo.ObjectID(fileId) }, { $set: { isPublic: false } });
+    const updatedFile = await dbClient.db.collection('files').findOne({ _id: new mongo.ObjectID(fileId) });
+
+    return res.status(200).json(updatedFile);
+  }
+
+  static async getFile(req, res) {
+    const fileId = req.params.id;
+    const file = await dbClient.db.collection('files').findOne({ _id: new mongo.ObjectID(fileId) });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    if (!file.isPublic) {
+      const token = req.headers['x-token'];
+      const key = `auth_${token}`;
+
+      const userId = await redisClient.get(key);
+      if (!userId || file.userId.toString() !== userId) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+    }
+
+    if (file.type === 'folder') {
+      return res.status(400).json({ error: "A folder doesn't have content" });
+    }
+
+    if (!fs.existsSync(file.localPath)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const mimeType = mime.lookup(file.name);
+    res.setHeader('Content-Type', mimeType);
+    const fileContent = fs.readFileSync(file.localPath);
+    return res.status(200).send(fileContent);
   }
 }
 
