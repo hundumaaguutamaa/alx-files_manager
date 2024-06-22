baa636c03b3ea4d6a0835c2cc439eab53fcd5f7c
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-22 04:59:52 +0300

    End point updated and controllers added

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
 const express = require('express');
-const router = express.Router(); // Initialize the router
+const router = express.Router(); // Initializing the router
 
-// Import your controllers (adjust paths as needed)
+// Import your controllers, adjust paths 
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 
-// Define your API endpoints
+// API endpoints definition
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', AuthController.getMe);
+router.get('/users/me', UsersController.getMe); // Adjusted to use UsersController
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
