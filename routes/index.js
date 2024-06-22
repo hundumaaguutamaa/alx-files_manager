8fd703d6201663516ee0c6e9a3144fa46f12e3c5
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-06-22 05:12:21 +0300

    Added file controller

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 
 const express = require('express');
-const router = express.Router(); // Initializing the router
+const router = express.Router();  
 
 // Import your controllers, adjust paths 
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');  
 
 // API endpoints definition
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe); // Adjusted to use UsersController
+router.get('/users/me', UsersController.getMe); 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/files', FilesController.postUpload); 
 
 module.exports = router;
