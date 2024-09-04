2959cc954abe21f9a0f0c1181f71e89f8d98776d
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-09-05 02:57:01 +0300

    Added json for parsing

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const routes = require('./routes/index');
+const redisClient = require('./utils/redis');  
+const dbClient = require('./utils/db');  
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Load all routes
 app.use('/', routes);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
-
