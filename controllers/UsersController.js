ae40489e95fd12306c4393508c5a70deb241f20d
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-09-05 03:40:25 +0300

    Update UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,27 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import Queue from 'bull';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const userQueue = new Queue('email sending');
 
 class UsersController {
   static async postNew(req, res) {
-    // (Existing code for POST /users)
+    // (Existing code for adding new user to DB)
+
+    try {
+      const newUser = await dbClient.db.collection('users').insertOne({
+        // User data from req.body
+      });
+
+      // Add job to userQueue for sending welcome email
+      await userQueue.add({
+        userId: newUser.insertedId
+      });
+
+      return res.status(201).json({ id: newUser.insertedId });
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   static async getMe(req, res) {
